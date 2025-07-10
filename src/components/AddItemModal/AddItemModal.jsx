05059eed65c5ee05594ec666af81cab424ae8954
cjrodriguez12 <120134645+cjrodriguez12@users.noreply.github.com>
2025-07-09 22:07:15 -0400

    Login Modal
    used addItemModal as template

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 const AddItemModal = ({ handleCloseModal, onAddItem, isOpen }) => {
---
+++ src/components/ModalLogin/ModalWithForm.jsx
@@ -0,0 +1,27 @@
+import "./ModalWithForm.css";
+
+const ModalWithForm = ({
+  children,
+  title,
+  onClose,
+  onSubmit,
+  name = "form",
+}) => {
+  return (
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal__content">
+        <div className="modal_header">
+          <button className="modal_button" type="button" onClick={onClose} />
+          <h3 className="modal_title">{title}</h3>
+        </div>
+        <form onSubmit={onSubmit} className="modal_form">
+          {children}
+          <button className="modal_submit" type="submit">
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default ModalWithForm;
