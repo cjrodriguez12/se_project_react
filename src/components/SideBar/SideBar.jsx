99ef19114de9968e4856a34ba6f2a2ed09af21e5
cjrodriguez12 <120134645+cjrodriguez12@users.noreply.github.com>
2026-02-12 23:11:24 -0500

    update Sidebar.jsx
    initial profile edit modal
    copied from signUp

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -10,6 +10,66 @@ const SideBar = () => {
           <img src={avatar} className="profile__avatar-img" alt="avatar" />
         </>
       </div>
+      <profileModalWithForm
+        title="Sign Up"
+        onClose={handleCloseModal}
+        isOpen={isOpen}
+        onSubmit={handleSubmit}
+        onLoginModal={onLoginModal}
+      >
+        <label className="modal_form-label">
+          Email
+          <input
+            className="modal_form-input"
+            placeholder="Email"
+            type="email"
+            name="email"
+            minLength="1"
+            maxLength="30"
+            value={email}
+            onChange={handleEmailChange}
+          ></input>
+        </label>
+        <label className="modal_form-label">
+          Password
+          <input
+            className="modal_form-input"
+            placeholder="Password"
+            type="password"
+            name="password"
+            minLength="1"
+            maxLength="300"
+            value={password}
+            onChange={handlePassChange}
+          ></input>
+        </label>
+        <label className="modal_form-label">
+          Name
+          <input
+            className="modal_form-input"
+            placeholder="Name"
+            type="text"
+            name="name"
+            minLength="1"
+            maxLength="30"
+            value={name}
+            onChange={handleNameChange}
+          ></input>
+        </label>
+        <label className="modal_form-label">
+          Avatar URL
+          <input
+            className="modal_form-input"
+            placeholder="Avatar URL"
+            type="url"
+            name="link"
+            minLength="1"
+            maxLength="300"
+            value={imageUrl}
+            onChange={handleUrlChange}
+          ></input>
+        </label>
+      </profileModalWithForm>
     </section>
   );
 };
